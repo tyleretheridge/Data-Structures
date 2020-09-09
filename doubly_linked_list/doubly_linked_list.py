aac866d1983237a2396b34531ccd97f65390019e
tyleretheridge <tylerjetheridge98@gmail.com>
2020-09-09 18:48:46 -0500

    update DLL

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -123,7 +123,7 @@ class DoublyLinkedList:
         self.add_to_head(value)
 
     """
-    Removes the input node from its current spot in the 
+    Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
